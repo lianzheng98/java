7e67d47b273bf0f1d2d520e04939c329b5f71a1c
lianzhengbj01 <lianzhengbj01@kanyun.com>
2023-05-28 23:42:03 +0800

    .gitignore

--- sb-web/src/main/java/com/pg/cc/sb/web/controller/TestController.java
+++ sb-web/src/main/java/com/pg/cc/sb/web/controller/TestController.java
@@ -6,13 +6,24 @@ package com.pg.cc.sb.web.controller;
  * @date 2020/12/14 6:46 下午
  */
 
+import com.pg.cc.sb.core.util.JSONUtils;
 import com.pg.cc.sb.web.annoation.Log2Center;
-import org.springframework.stereotype.Controller;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@RestController
 @RequestMapping("/app")
+@Slf4j
 public class TestController {
 
     @RequestMapping("/test")
@@ -22,4 +33,24 @@ public class TestController {
         String springframework = "213";
         return springframework;
     }
+
+
+    @Data
+    public static class TestValidationCls {
+
+        @Min(1)
+        @NotNull(message = "用户id不能为空")
+        int id;
+
+        @NotBlank(message = "name 不能为null")
+        String name;
+
+    }
+
+    @PostMapping("/e1")
+    public String e1(@RequestBody @Validated TestValidationCls testValidationCls) {
+        log.info(JSONUtils.writeValue(testValidationCls));
+        return JSONUtils.writeValue(testValidationCls);
+    }
+
 }
